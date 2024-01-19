3a4a230014b83c0550c9379e48bea6d7b877fec4
tevo.castro14@gmail.com <tevo.castro14@gmail.com>
2024-01-19 12:49:38 -0300

    novo

---
+++ javascript.js
@@ -0,0 +1,10 @@
+const inputNome = document.getElementById('nome').value
+const inputTelefone = document.getElementById('telefone').value
+const form = document.getElementById('form-cadastrar')
+
+form.addEventListener('submit', function(e){
+  e.preventDefault;
+  
+  linha = "tr"
+  
+})
