18aad1fd7f6507eb1b898db5d9b2a3638d8ddbf9
tevo.castro14@gmail.com <tevo.castro14@gmail.com>
2024-01-19 17:27:23 -0300

    Java script completo

--- javascript.js
+++ javascript.js
@@ -1,10 +1,38 @@
-const inputNome = document.getElementById('nome').value
-const inputTelefone = document.getElementById('telefone').value
-const form = document.getElementById('form-cadastrar')
+const inputNome = document.getElementById('nome');
+const inputTelefone = document.getElementById('telefone');
+const form = document.getElementById('form-cadastrar');
+linhas = '';
+const nome = [];
+const telefone = [];
 
-form.addEventListener('submit', function(e){
-  e.preventDefault;
-  
-  linha = "tr"
-  
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  adicionarLinha();
+  atualizarTabela();
 })
+
+function adicionarLinha() {
+
+  if (nome.includes(inputNome.value) || telefone.includes(parseInt(inputTelefone.value  ))) {
+    
+    alert(`O nome: ${inputNomeAtividade.value} ja foi inserida`);
+  } else {
+    nome.push(inputNome.value);
+    telefone.push(parseInt(inputTelefone.value));
+
+    let linha = '<tr>';
+    linha += `<td>${inputNome.value}</td>`;
+    linha += `<td>${inputTelefone.value}</td>`;
+    linha += '</tr>';
+
+    linhas += linha
+  }
+  inputTelefone.value = '';
+  inputNome.value = '';
+}
+
+function atualizarTabela() {
+  const corpoTabela = document.querySelector('tbody');
+  corpoTabela.innerHTML = linhas;
+}
